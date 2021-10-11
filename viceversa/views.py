bcc30a67c4af66827efcb805215c285539dcf53c
Max Silchenko <maksim379@yandex.ru>
2021-10-11 21:19:26 +0300

    Вывод количество слов в форме

--- viceversa/views.py
+++ viceversa/views.py
@@ -7,5 +7,11 @@ def home(request):
 def reverse(request):
     user_text = request.GET['usertext']
     reversed_text = ''.join(reversed(user_text))
+    skip = user_text.count(' ')
+    skip +=1
+    if skip > 1:
+        word = 'words'
+    else:
+        word = 'word'
     return render(request, 'reverse.html', {'usertext': user_text,
-         'reversedtext': reversed_text})
+         'reversedtext': reversed_text, 'total_words': skip, 'word': word })
